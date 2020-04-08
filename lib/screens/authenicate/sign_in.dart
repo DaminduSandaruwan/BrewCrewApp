621ab14a6a677305a37725d630f0a5bcc6978249
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-08 14:03:30 +0530

    Toggle Between Forms

--- lib/screens/authenicate/sign_in.dart
+++ lib/screens/authenicate/sign_in.dart
@@ -2,6 +2,10 @@ import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
+
+  final Function toggleView;
+  SignIn({this.toggleView});
+
   @override
   _SignInState createState() => _SignInState();
 }
@@ -18,10 +22,19 @@ String password = '';
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.brown[100],
-      appBar: AppBar(
+      appBar: AppBar(        
         backgroundColor: Colors.brown[400],
         elevation: 0.0,
         title: Text('Sign in to Brew Crew'),
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('Register'),
+            onPressed: (){
+              widget.toggleView();
+            },
+          )
+        ],
       ),
       body:Container(
         padding: EdgeInsets.symmetric(vertical:20.0, horizontal:50.0),
