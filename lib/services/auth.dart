46c9a2c141b1fdda8e7d0aca52139ec0838ec2c5
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-09 00:40:25 +0530

    Firestore Database Done - errors fixed

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,4 +1,5 @@
 import 'package:brew_crew/models/user.dart';
+import 'package:brew_crew/services/database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService{
@@ -54,6 +55,10 @@ class AuthService{
     try{
       AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
       FirebaseUser user = result.user;
+
+  //create a new document for the user with the uid
+      await DatabaseService(uid: user.uid).updateUserData('0', 'new crew member', 100);
+
       return _userFromFirebaseUser(user);
       
     }catch(e){
