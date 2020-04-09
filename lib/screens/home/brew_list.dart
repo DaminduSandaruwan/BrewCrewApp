94bba47141137871bbf67f37f4d941d769e0fd39
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-09 22:05:42 +0530

    Listing Brew Data

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -1,4 +1,5 @@
 import 'package:brew_crew/models/brew.dart';
+import 'package:brew_crew/screens/home/brew_tile.dart';
 import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
@@ -24,7 +25,11 @@ class _BrewListState extends State<BrewList> {
       print(brews.strength);
     });
 
-    return Container(
+    return ListView.builder(
+      itemCount: brews.length,
+      itemBuilder: (context,index){
+        return BrewTile(brew:brews[index]);
+      },
       
     );
   }
