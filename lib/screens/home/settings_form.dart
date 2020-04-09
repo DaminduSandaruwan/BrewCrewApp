cfbc95958f3e18ea07e9375cf3fd9c20e5f2c4a0
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-09 23:15:23 +0530

    Slider Added

--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -45,6 +45,15 @@ class _SettingsFormState extends State<SettingsForm> {
             onChanged: (val) => setState(()=> _currentSugars=val)
           ),
           //Slider
+          Slider(
+            value: (_currentStrength ?? 100).toDouble(),
+            activeColor: Colors.brown[_currentStrength ?? 100],
+            inactiveColor: Colors.brown[_currentStrength ?? 100],
+            min: 100.0,
+            max: 900.0,
+            divisions: 8,
+            onChanged: (val)=> setState(()=> _currentStrength=val.round()),
+          ),
           //Button
           RaisedButton(
             color: Colors.pink[400],
