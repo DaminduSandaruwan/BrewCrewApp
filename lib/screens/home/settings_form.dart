d46905d89710e34cebee0b951f5f224891bd28bd
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-10 10:54:57 +0530

    User Data Model and Stream Builder

--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -1,5 +1,9 @@
+import 'package:brew_crew/models/user.dart';
+import 'package:brew_crew/services/database.dart';
+import 'package:brew_crew/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:brew_crew/shared/constant.dart';
+import 'package:provider/provider.dart';
 
 class SettingsForm extends StatefulWidget {
   @override
@@ -17,58 +21,74 @@ class _SettingsFormState extends State<SettingsForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: <Widget>[
-          Text(
-            'Update your brew settings.',
-            style: TextStyle(fontSize: 18.0),
+    final user = Provider.of<User>(context);  
+
+    return StreamBuilder<UserData>(
+      stream: DatabaseService(uid: user.uid).userData,
+      builder: (context, snapshot) {
+        if(snapshot.hasData){
+          UserData userData = snapshot.data;
+
+          return Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              Text(
+                'Update your brew settings.',
+                style: TextStyle(fontSize: 18.0),
+              ),
+              SizedBox(height: 20.0),
+              TextFormField(
+                initialValue: userData.name,
+                decoration: textInputDecoration,
+                validator: (val) => val.isEmpty ? 'Please enter a name' : null,
+                onChanged: (val) => setState(() => _currentName=val),            
+              ),
+              SizedBox(height: 20.0),
+              //DropDown
+              DropdownButtonFormField(
+                decoration: textInputDecoration,
+                value: _currentSugars ?? userData.sugars,
+                items: sugars.map((sugar){
+                  return DropdownMenuItem(
+                    child: Text('$sugar sugars'),
+                    value: sugar,
+                  );
+                }).toList(), 
+                onChanged: (val) => setState(()=> _currentSugars=val)
+              ),
+              //Slider
+              Slider(
+                value: (_currentStrength ?? userData.strength).toDouble(),
+                activeColor: Colors.brown[_currentStrength ?? userData.strength],
+                inactiveColor: Colors.brown[_currentStrength ?? userData.strength],
+                min: 100.0,
+                max: 900.0,
+                divisions: 8,
+                onChanged: (val)=> setState(()=> _currentStrength=val.round()),
+              ),
+              //Button
+              RaisedButton(
+                color: Colors.pink[400],
+                child: Text(
+                  'Update',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () async {
+                  print(_currentName);
+                  print(_currentSugars);
+                  print(_currentStrength);
+                },
+              )
+            ],
           ),
-          SizedBox(height: 20.0),
-          TextFormField(
-            decoration: textInputDecoration,
-            validator: (val) => val.isEmpty ? 'Please enter a name' : null,
-            onChanged: (val) => setState(() => _currentName=val),            
-          ),
-          SizedBox(height: 20.0),
-          //DropDown
-          DropdownButtonFormField(
-            decoration: textInputDecoration,
-            value: _currentSugars ?? '0',
-            items: sugars.map((sugar){
-              return DropdownMenuItem(
-                child: Text('$sugar sugars'),
-                value: sugar,
-              );
-            }).toList(), 
-            onChanged: (val) => setState(()=> _currentSugars=val)
-          ),
-          //Slider
-          Slider(
-            value: (_currentStrength ?? 100).toDouble(),
-            activeColor: Colors.brown[_currentStrength ?? 100],
-            inactiveColor: Colors.brown[_currentStrength ?? 100],
-            min: 100.0,
-            max: 900.0,
-            divisions: 8,
-            onChanged: (val)=> setState(()=> _currentStrength=val.round()),
-          ),
-          //Button
-          RaisedButton(
-            color: Colors.pink[400],
-            child: Text(
-              'Update',
-              style: TextStyle(color: Colors.white),
-            ),
-            onPressed: () async {
-              print(_currentName);
-              print(_currentSugars);
-              print(_currentStrength);
-            },
-          )
-        ],
-      ),
+        );
+
+        }else{
+          return Loading();
+        }
+        
+      }
     );
   }
 }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,4 +1,5 @@
 import 'package:brew_crew/models/brew.dart';
+import 'package:brew_crew/models/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService{
@@ -30,10 +31,29 @@ class DatabaseService{
     }).toList();
   }
 
+  // user data from snapshot
+  UserData _userDataFromSnapshot(DocumentSnapshot snapshot){
+    return UserData(
+      uid: uid,
+      name: snapshot.data['name'],
+      sugars: snapshot.data['sugars'],
+      strength: snapshot.data['strength']
+    );
+  }
+
+
   //get brews stream
 
   Stream<List<Brew>> get brews{
     return brewCollection.snapshots()
     .map(_brewListFromSnapshot);
   }
+
+  //get user doc stream
+  Stream<UserData> get userData {
+    return brewCollection.document(uid).snapshots()
+    .map(_userDataFromSnapshot);
+  }
+
+
 }
